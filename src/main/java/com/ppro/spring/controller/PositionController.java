5c8ef90456354a3d3179dc70299a7832a4ef3cea
dell <icsifmhprt@email.cz>
2015-01-01 22:29:18 +0100

    zjištění pozice google.com - nedokončeno zjištění pozice centrum.cz - dokončeno

--- src/main/java/com/ppro/spring/controller/PositionController.java
+++ src/main/java/com/ppro/spring/controller/PositionController.java
@@ -30,6 +30,27 @@ public class PositionController
         int number_of_pages = Integer.parseInt(numberOfPage);
 
         // Seznam.cz
+        int seznam_position = getSeznamPosition(url, key, number_of_pages);
+
+        // Google.com
+        int google_position = getGooglePosition(url, key, number_of_pages);
+
+        // Centrum.cz
+        int centrum_position = getCentrumPosition(url, key, number_of_pages);
+
+        Elements elements = HtmlParser.getElements("http://www.google.cz/search?q="+key+"&start=", 0, 10, number_of_pages, "a");
+        List<String> links = HtmlParser.getAttributes(elements, "href");
+
+        model.addAttribute("resultList", links);
+        model.addAttribute("seznam_position", seznam_position);
+        model.addAttribute("google_position", google_position);
+        model.addAttribute("centrum_position", centrum_position);
+        model.addAttribute("pageName", "results");
+        return "template";
+	}
+
+    public int getSeznamPosition(String url, String key, int number_of_pages)
+    {
         Elements elements = HtmlParser.getElements("http://search.seznam.cz/?q="+key+"&count=10&from=", 0, 10, number_of_pages, ".info a");
         List<String> links = HtmlParser.getAttributes(elements, "href");
 
@@ -43,9 +64,42 @@ public class PositionController
             }
         }
 
-        model.addAttribute("resultList", links);
-        model.addAttribute("position", position);
-        model.addAttribute("pageName", "results");
-        return "template";
-	}
+        return position;
+    }
+//    https://www.google.cz/search?q=auto.cz&start=10
+    public int getGooglePosition(String url, String key, int number_of_pages)
+    {
+        Elements elements = HtmlParser.getElements("https://www.google.cz/search?q="+key+"&start=", 0, 10, number_of_pages, ".r a");
+        List<String> links = HtmlParser.getAttributes(elements, "href");
+
+        int position = 0;
+
+        for (int i = 0; i < links.size(); i++)
+        {
+            if (links.get(i).contains(url))
+            {
+                position = i + 1;
+            }
+        }
+
+        return position;
+    }
+
+    public int getCentrumPosition(String url, String key, int number_of_pages)
+    {
+        Elements elements = HtmlParser.getElements("http://search.centrum.cz/index.php?q="+key+"&from=", 0, 10, number_of_pages, "h3 a");
+        List<String> links = HtmlParser.getAttributes(elements, "href");
+
+        int position = 0;
+
+        for (int i = 0; i < links.size(); i++)
+        {
+            if (links.get(i).contains(url))
+            {
+                position = i + 1;
+            }
+        }
+
+        return position;
+    }
 }
